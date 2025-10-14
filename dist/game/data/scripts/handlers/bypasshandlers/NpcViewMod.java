68a7855ffe9e425dacde718c3a201e2bdb2e390d
MobiusDevelopment <mobius@cyber-wizard.com>
2025-10-15 00:24:03 +0300

    Do not display zero chance drops at NpcViewMod html.

--- dist/game/data/scripts/handlers/bypasshandlers/NpcViewMod.java
+++ dist/game/data/scripts/handlers/bypasshandlers/NpcViewMod.java
@@ -554,6 +554,12 @@ public class NpcViewMod implements IBypassHandler
 				rateChance *= dropRateEffectBonus;
 			}
 			
+			// Do not display zero chance drops.
+			if (rateChance == 0d)
+			{
+				continue;
+			}
+			
 			sb.append("<table width=332 cellpadding=2 cellspacing=0 background=\"L2UI_CT1.Windows.Windows_DF_TooltipBG\">");
 			sb.append("<tr><td width=32 valign=top>");
 			sb.append("<img src=\"" + (item.getIcon() == null ? "icon.etc_question_mark_i00" : item.getIcon()) + "\" width=32 height=32>");
